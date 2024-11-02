f77ab5e21d40e203b1bdb723201a854203ff3c4f
Xun <xun.li@student.maastrichtuniversity.nl>
2024-11-03 00:13:35 +0100

    up

--- Backpropogation.py
+++ Backpropogation.py
@@ -8,70 +8,70 @@ def sigmoid_derivative(x):
 
 class layer:
     def __init__(self, input_size, output_size):
-        self.weights = np.random.rand(input_size, output_size)
-        self.bias = np.random.rand(1, output_size)
+        self.weights = np.random.rand(input_size + 1, output_size)  # +1 for bias
         self.output = None
         self.input = None
         self.delta = None
         self.delta_weights = None
-        self.delta_bias = None
 
     def forward(self, input):
-        self.input = input
-        self.output = sigmoid(np.dot(input, self.weights) + self.bias)
+        # Add bias term to the input
+        self.input = np.concatenate((input, np.ones((input.shape[0], 1))), axis=1)
+        self.output = sigmoid(np.dot(self.input, self.weights))
         return self.output
 
     def backward(self, delta):
-        self.delta = delta
-        self.delta_weights = np.dot(self.input.T, delta)
-        self.delta_bias = np.sum(delta, axis=0, keepdims=True)
-        return np.dot(delta, self.weights.T)
+        # Calculate delta weights including bias
+        self.delta_weights = np.dot(self.input.T, delta * sigmoid_derivative(self.output))
+        self.delta = np.dot(delta, self.weights.T)[:, :-1]  # Exclude bias from delta
 
     def update(self, learning_rate):
         self.weights += learning_rate * self.delta_weights
-        self.bias += learning_rate * self.delta_bias
 
-inputlayer = layer(8, 8)
 hiddenlayer = layer(8, 3)
 outputlayer = layer(3, 8)
 
-input = np.array([[0,0,0,0,0,0,0,1],
-                     [0,0,0,0,0,0,1,0],
-                     [0,0,0,0,0,1,0,0],
-                     [0,0,0,0,1,0,0,0],
-                     [0,0,0,1,0,0,0,0],
-                     [0,0,1,0,0,0,0,0],
-                     [0,1,0,0,0,0,0,0],
-                     [1,0,0,0,0,0,0,0]])
+input = np.array([[0, 0, 0, 0, 0, 0, 0, 1],
+                  [0, 0, 0, 0, 0, 0, 1, 0],
+                  [0, 0, 0, 0, 0, 1, 0, 0],
+                  [0, 0, 0, 0, 1, 0, 0, 0],
+                  [0, 0, 0, 1, 0, 0, 0, 0],
+                  [0, 0, 1, 0, 0, 0, 0, 0],
+                  [0, 1, 0, 0, 0, 0, 0, 0],
+                  [1, 0, 0, 0, 0, 0, 0, 0]])
 
-output = np.array([[0,0,0,0,0,0,0,1],
-                     [0,0,0,0,0,0,1,0],
-                     [0,0,0,0,0,1,0,0],
-                     [0,0,0,0,1,0,0,0],
-                     [0,0,0,1,0,0,0,0],
-                     [0,0,1,0,0,0,0,0],
-                     [0,1,0,0,0,0,0,0],
-                     [1,0,0,0,0,0,0,0]])
+output = np.array([[0, 0, 0, 0, 0, 0, 0, 1],
+                   [0, 0, 0, 0, 0, 0, 1, 0],
+                   [0, 0, 0, 0, 0, 1, 0, 0],
+                   [0, 0, 0, 0, 1, 0, 0, 0],
+                   [0, 0, 0, 1, 0, 0, 0, 0],
+                   [0, 0, 1, 0, 0, 0, 0, 0],
+                   [0, 1, 0, 0, 0, 0, 0, 0],
+                   [1, 0, 0, 0, 0, 0, 0, 0]])
 
 learning_rate = 0.1
 
-# for i in range(10000):
-#     for j in range(8):
-#         inputlayer.forward(input[j:j+1])
-#         hiddenlayer.forward(inputlayer.output)
-#         outputlayer.forward(hiddenlayer.output)
+# Example training loop
+for i in range(10000):
+    hidden_output = hiddenlayer.forward(input)
+    final_output = outputlayer.forward(hidden_output)
+    
+    # Calculate error
+    error = output - final_output
+    
+    # Backpropagation
+    outputlayer.backward(error)
+    hiddenlayer.backward(outputlayer.delta)
+    
+    # Update weights
+    outputlayer.update(learning_rate)
+    hiddenlayer.update(learning_rate)
 
-#         outputlayer.backward(sigmoid_derivative(outputlayer.output) * (output[j:j+1] - outputlayer.output))
-#         hiddenlayer.backward(outputlayer.delta)
-#         inputlayer.backward(hiddenlayer.delta)
+    if i % 1000 == 0:
+        print("Epoch: ", i)
+        print(outputlayer.output)
+        print("Error: ", np.mean(np.square(output - outputlayer.output)))
 
-#         outputlayer.update(learning_rate)
-#         hiddenlayer.update(learning_rate)
-#         inputlayer.update(learning_rate)
 
-#     if i % 1000 == 0:
-#         print("Error: ", np.mean(np.square(output - outputlayer.output)))
-
-print((input[1:3]))
 
     
